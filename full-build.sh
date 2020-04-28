ffafb509d08308b585af09f85b409c6c6ff676dd
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2020-04-28 01:42:55 -0500

    do not run PKGBUILD check()

--- full-build.sh
+++ full-build.sh
@@ -10,9 +10,12 @@ pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{gcc,gcc-fortran}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{icu,libtiff,libjpeg,libpng,pcre2,xz,bzip2,zlib}
 pacman -S --needed --noconfirm mingw-w64-{i686,x86_64}-{cairo,tk,curl}
 
+# Users who do not need it . . . call makepkg with --nocheck flag
+# https://wiki.archlinux.org/index.php/Creating_packages#check()
+#
 # Build package (only once)
 set -o pipefail
-MINGW_INSTALLS="mingw64" makepkg-mingw 2>&1 | tee r-devel.log
+MINGW_INSTALLS="mingw64" makepkg-mingw --nocheck 2>&1 | tee r-devel.log
 
 # Copy installer to root directory
 cp -f src/R-source/src/gnuwin32/installer/*.exe .
